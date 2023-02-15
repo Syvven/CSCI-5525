c3e922189a0292a12a1fee22f8ed03af92b05710
Noah Hendrickson <nhendrickson2001@gmail.com>
2023-02-15 15:23:02 -0600

    updated svm

--- homework2/MySVM.py
+++ homework2/MySVM.py
@@ -11,12 +11,19 @@ class MySVM:
         self.b = None
 
     def fit(self, X, y):
-        self.w = np.random.uniform(-0.01, 0.01, X.shape[1])
+        self.w = self.w = np.random.uniform(-0.01, 0.01, X.shape[1])
         self.b = np.random.uniform(-0.01, 0.01)
 
         for _ in range(self.iters):
+            # for j in range(len(w)):
+
             for i in range(len(X)):
-                pass
+                condition = (1 + -1*y[i]*(np.dot(self.w, X[i]) + self.b)) <= 0
+                if condition:
+                    self.w -= self.lr * self.w
+                else:
+                    self.w -= self.lr * (self.w - self.c * np.dot(y[i], X[i]))
+                    self.b -= self.lr * -1*y[i]
         
 
     def predict(self, X):
